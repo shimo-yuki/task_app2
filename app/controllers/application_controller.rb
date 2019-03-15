ee777c1ad05c82137ddddbf61b8b0b873b976b18
shimomurayuki <c0116138b7@edu.teu.ac.jp>
2019-03-15 20:01:43 +0900

    0315-4 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  def after_sign_up_path_for(resource)
+    root_path # ログイン後に遷移するpathを設定
+  end
 
 protected
 
